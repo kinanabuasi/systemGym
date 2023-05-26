eb07e11fb74f1d5eafc8fabbddea36fc2e50f4ab
Team-etrevago <infoalaseer@gmail.com>
2023-05-26 06:00:56 -0700

    add informations screens

--- lib/view/screens/auth/player_auth/choose_gender_screen.dart
+++ lib/view/screens/auth/player_auth/choose_gender_screen.dart
@@ -6,6 +6,7 @@ import 'package:systemgym/logic/controllers/auth/player_auth/choose_gender_contr
 
 import '../../../../component/buttons/my_buttons.dart';
 import '../../../../constants/routes.dart';
+import '../../../widgets/auth_player/take_information_body.dart';
 
 GenderController genderController = Get.put(GenderController());
 
@@ -14,113 +15,61 @@ class ChooseGenderScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-      backgroundColor: mainColor,
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Column(
-              children: [
-                MyText(
-                    text: "Tell us about yourself!",
-                    color: white,
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold),
-                const SizedBox(
-                  height: 10,
-                ),
-                MyText(
-                    textAlign: TextAlign.center,
-                    text:
-                        "To give you a better experience we need to know your gender",
-                    color: white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold),
-              ],
-            ),
-            GetBuilder<GenderController>(
-                init: GenderController(),
-                builder: (genderController) {
-                  return Column(
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          genderController.isFemalePressed = false;
-                          genderController.isMalePressed = true;
-                          genderController.update();
-                        },
-                        child: CircleAvatar(
-                          backgroundColor: genderController.isMalePressed
-                              ? yellowColor
-                              : darkGrey,
-                          radius: 70,
-                          child: Icon(
-                            Icons.male,
-                            color: genderController.isMalePressed
-                                ? mainColor
-                                : white,
-                            size: 60,
-                          ),
-                        ),
+    return TakeInformationBody(
+        infoText: "To give you a better experience we need to know your gender",
+        screenHead: "Tell us about yourself!",
+        nextButtonFunction: () {
+          Get.toNamed(Routes.chooseAge);
+        },
+        screenBody: GetBuilder<GenderController>(
+            init: GenderController(),
+            builder: (genderController) {
+              return Column(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      genderController.isFemalePressed = false;
+                      genderController.isMalePressed = true;
+                      genderController.update();
+                    },
+                    child: CircleAvatar(
+                      backgroundColor: genderController.isMalePressed
+                          ? yellowColor
+                          : darkGrey,
+                      radius: 70,
+                      child: Icon(
+                        Icons.male,
+                        color:
+                            genderController.isMalePressed ? mainColor : white,
+                        size: 60,
                       ),
-                      const SizedBox(
-                        height: 40,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      genderController.isMalePressed = false;
+                      genderController.isFemalePressed = true;
+                      genderController.update();
+                    },
+                    child: CircleAvatar(
+                      backgroundColor: genderController.isFemalePressed
+                          ? yellowColor
+                          : darkGrey,
+                      radius: 70,
+                      child: Icon(
+                        Icons.female,
+                        color: genderController.isFemalePressed
+                            ? mainColor
+                            : white,
+                        size: 60,
                       ),
-                      InkWell(
-                        onTap: () {
-                          genderController.isMalePressed = false;
-
-                          genderController.isFemalePressed = true;
-                          genderController.update();
-                        },
-                        child: CircleAvatar(
-                          backgroundColor: genderController.isFemalePressed
-                              ? yellowColor
-                              : darkGrey,
-                          radius: 70,
-                          child: Icon(
-                            Icons.female,
-                            color: genderController.isFemalePressed
-                                ? mainColor
-                                : white,
-                            size: 60,
-                          ),
-                        ),
-                      ),
-                    ],
-                  );
-                }),
-            Row(
-              children: [
-                Spacer(),
-                SizedBox(
-                  height: 50,
-                  width: 125,
-                  child: MyButton(
-                      color: yellowColor,
-                      widget: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          MyText(
-                              text: "Next",
-                              color: mainColor,
-                              fontSize: 17,
-                              fontWeight: FontWeight.bold),
-                          const Icon(Icons.chevron_right, color: mainColor)
-                        ],
-                      ),
-                      function: () {
-                        Get.toNamed(Routes.chooseAge);
-                      }),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
-    ));
+                    ),
+                  ),
+                ],
+              );
+            }));
   }
 }
