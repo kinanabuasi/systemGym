3697af2fc3401f17bf7823c2b59cf58b90e584e0
Team-etrevago <infoalaseer@gmail.com>
2023-07-02 14:58:49 -0700

    add team screen

---
+++ lib/logic/bindings/public/add_team_binding.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+import '../../controllers/other/add_products_controller.dart';
+import '../../controllers/public/add_team_controller.dart';
+
+class AddTeamBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut(() => AddTeamController());
+  }
+}
---
+++ lib/logic/controllers/public/add_team_controller.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class AddTeamController extends GetxController {
+  List<String> coaches = ['coach 1', 'coach 2', 'coach 3'];
+  String? teamCoach;
+
+  List<String> members = ['member 1', 'member 2', 'member 3'];
+  String? teamMember;
+}
